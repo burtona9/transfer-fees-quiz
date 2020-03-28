73d77fb140f2727b3d81e4cf678b23257bb177b1
Adam Burton <adamburton27@hotmail.com>
2020-03-28 21:46:07 +0000

    vue set up

---
+++ src/game.js
@@ -0,0 +1,11 @@
+
+
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    var app = new Vue({
+        el: '#app',
+        data: {
+          message: 'Hello Vue!'
+        }
+      })
+  })
