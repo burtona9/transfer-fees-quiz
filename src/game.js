3f6e45d3b79c6756ea131233e6cb9f3f28cbe223
Adam Burton <adamburton27@hotmail.com>
2020-05-10 01:17:59 +0100

    questions restructured

--- src/game.js
+++ src/game.js
@@ -5,7 +5,29 @@
     var app = new Vue({
         el: '#app',
         data: {
-          message: 'Hello Vue!'
+          questions: questions,
+          currentQuestionIndex: 0,
+          score: 0,
+          gameFinished: false
+
+        },
+        methods: {
+        handleAnswerSubmission(ans){
+            if(this.questions[this.currentQuestionIndex].answers[ans].correct)
+                this.score ++;
+
+            
+            this.currentQuestionIndex ++;
+            if(this.currentQuestionIndex === this.questions.length){
+              this.currentQuestionIndex = 0;
+              this.gameFinished = true;
+            }
         }
+    },
+    computed: {
+      currentQuestion(){
+        return this.questions[this.currentQuestionIndex];
+      }
+    }
       })
   })
---
+++ src/questions.js
@@ -0,0 +1,38 @@
+const questions = [ {
+    "question" : "What was the score in the 2003 FA Cup final between Aresenal and Southampton?",
+    "answers": [
+        {"option":"Arseanl 1 - Southampton 0","correct":true},
+        {"option":"Arseanl 2 - Southampton 1", "correct":false},
+        {"option":"Arseanl 2 - Southampton 0","correct":false},
+        {"option":"Arseanl 3 - Southampton 1", "correct":false}
+    ]
+},
+{
+    "question" : "How many substitutes will Premier leagues sides be allowed to make in the remaining games of the 2019-20 season?",
+    "answers": [
+        {"option":"5","correct":true},
+        {"option":"4", "correct":false},
+        {"option":"3","correct":false},
+        {"option":"6", "correct":false}
+    ]
+},
+{
+    "question" : "Which three teams are currently in the relegation zone?",
+    "answers": [
+        {"option":"Norwich City, Aston Villa, Bournemouth","correct":true},
+        {"option":"Aston Villa, Newcastle Utd, Norwich City","correct":false},
+        {"option":"Southampton, Aston Villa, Norwich City", "correct":false},  
+        {"option":"Norwich City, Watford, West Ham Utd", "correct":false}
+    ]
+},
+{
+    "question" : "Which country did England draw 0-0 with in the World Cup Group 2010 group phase?",
+    "answers": [
+        {"option":"Algeria", "correct":true},
+        {"option":"Costa Rica","correct":false},
+        {"option":"Sloviena", "correct":false},  
+        {"option":"Sweden", "correct":false}
+    ]
+}
+
+]
